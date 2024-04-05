9eba26c1ae6d906d63d6850e86bf16e07c8c2137
kendell <kenren@udel.edu>
2024-04-05 08:59:37 -0400

    added 5 day forcast

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+const baseURL='https://api.openweathermap.org/data/2.5/';
+const options ='weather?q=atlanta&appid='
+//const options ='weather?q=atlanta&appid=1f965d36035e44dfd7ce52a501d2500e'
+const apikey=`1f965d36035e44dfd7ce52a501d2500e`
+const form=$('form');
+
+
+
+  async function  runSearch(event){
+    event.preventDefault()
+    const input=$('#search_box').val()
+    const searchResult= await getWeather(input)
+    console.log(searchResult)
+    const Name=searchResult.weather.name
+    const Temperture=searchResult.weather.main.temp
+    const TempertureMax=searchResult.weather.main.temp_max
+    const TempertureMin=searchResult.weather.main.temp_min
+    const Windspeed=searchResult.weather.wind.speed
+    const humidity= searchResult.weather.main.humidity;
+    const WeatherOBJ={
+      Name:Name,
+      Temperture:Temperture,
+      TempertureMax:TempertureMax,
+      TempertureMin:TempertureMin,
+      Windspeed:Windspeed,
+      humidity:humidity
+    }
+    
+    PostWeather(WeatherOBJ)
+   // return WeatherOBJ
+  }
+
+  async function getWeather(cityname) {
+    // const response = await fetch('https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={1f965d36035e44dfd7ce52a501d2500e}');
+    const country=`UK`
+    const url=baseURL+`weather?q=${cityname},&appid=`+apikey+`&units=imperial`
+    const urlForcast=`https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=${apikey}&units=imperial`;
+    let weatherOBJ={
+      weather:null,
+      forcast:null
+    }
+    let response = await fetch(url);
+     weatherOBJ.weather= await response.json();
+
+    response=await fetch(urlForcast);
+    weatherOBJ.forcast=filterTime(await response.json());
+
+    //filterTime(weatherOBJ.forcast)
+    return weatherOBJ
+  }
+
+
+  function filterTime(OBJ){
+    let arr=[]
+      OBJ.list.forEach(element => {
+        if(element.dt_txt.substr(11)=="12:00:00"){
+          arr.push(element)
+        }
+      });
+      //console.log(arr) 
+      return arr
+  }
+
+  function PostWeather(OBJ){
+    let Title = document.getElementById('City')
+    let Temp = document.getElementById('temp')
+    let Wind = document.getElementById('wind')
+    let humidity=document.getElementById('humidity')
+    Title.innerText=OBJ.Name
+    Temp.innerText="Temperture: "+OBJ.Temperture+" F"
+    Wind.innerText="Wind Speed: "+OBJ.Windspeed+" MPH"
+    humidity.innerText="Humidity: "+OBJ.humidity+" %"
+    console.log(OBJ)
+
+  }
+
+
+
+  form.on('submit',runSearch)
+  
